79ff0dd3643e65c84870f8be5b15e023587fece5
hummus-erectus <robert.samuel.grayson@gmail.com>
2022-11-14 14:01:50 +0900

    Trying to implement audio

--- src/Components/QuestionScreen.jsx
+++ src/Components/QuestionScreen.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import VoiceRecorder from "./VoiceRecorder"
 
 export default function QuestionScreen (props) {
 
     const [prepTime, setPrepTime] = React.useState(true)
+    const [userSpeaking, setUserSpeaking] = React.useState(false)
     const [prepCounter, setPrepCounter] = React.useState(props.customTime.prepTime)
     const [speakCounter, setSpeakCounter] = React.useState(props.customTime.speakTime)
     const question = props.questionText
@@ -15,22 +17,34 @@ export default function QuestionScreen (props) {
                 function delay(time) {
                     return new Promise(resolve => setTimeout(resolve, time))
                 }
-            delay(1000).then(() => setPrepTime(false))
+            delay(1000).then(() => {
+                setPrepTime(false)
+                setUserSpeaking(true)
+            })
             }else{
-                setPrepTime(false) 
+                setPrepTime(false)
+                setUserSpeaking(true) 
             }
         }
       }, [prepCounter]
     );
 
     React.useEffect(() => {
-        if (!prepTime){
-            speakCounter > 0 && setTimeout(() => setSpeakCounter(speakCounter - 1), 1000)
+        if (userSpeaking){
+            speakCounter > 0 ? setTimeout(() => setSpeakCounter(speakCounter - 1), 1000) : setTimeout(() => setUserSpeaking(false), 1000)
         }
-      }, [prepTime, speakCounter]
-    );
+      }, [userSpeaking, speakCounter]
+    )
+
+    // React.useEffect(() => {
+    //     if (speakCounter===0){
+    //         setTimeout(() => setUserSpeaking(false), 3000)
+    //     }
+    //   }, [userSpeaking, speakCounter]
+    // )
 
     function handleRestartButtonClick(){
+        setUserSpeaking(true)
         setSpeakCounter(props.customTime.speakTime)
     }
 
@@ -41,7 +55,7 @@ export default function QuestionScreen (props) {
     
 
     return(
-        <div className='question--container'>
+        <div id="q-container" className='question--container'>
             <h1 className='question--text'>{question}</h1>
             {prepTime ?
                 <h2 className='question--counter'>Preparation time remaining: {prepCounter}</h2>
@@ -50,10 +64,15 @@ export default function QuestionScreen (props) {
             }
             <div className='question--buttons-container'>
                 <button className="choose-again-button" onClick={handleChooseButtonClick}>Choose a Different Question</button>
-                {speakCounter===0 && 
+                {(speakCounter===0 && userSpeaking===false) && 
                     <button className="restart-button" onClick={handleRestartButtonClick}>Restart Speaking Timer</button>
                 } 
             </div>
+            <VoiceRecorder
+                prepTime={prepTime}
+                userSpeaking={userSpeaking}
+                speakCounter={speakCounter}
+            />
         </div>
     )
 }
---
+++ src/Components/VoiceRecorder.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { AudioRecorder, useAudioRecorder } from 'react-audio-voice-recorder'
+
+export default function VoiceRecorder (props) {
+    const recorderControls = useAudioRecorder()
+    const addAudioElement = (blob) => {
+        const url = URL.createObjectURL(blob)
+        const audio = document.createElement("audio")
+        audio.src = url
+        audio.controls = true
+        document.getElementById("q-container").appendChild(audio)
+      }
+
+      React.useEffect(() => {
+        if (props.userSpeaking){
+            recorderControls.startRecording()
+        }
+      }, [props.userSpeaking]) 
+
+      React.useEffect(() => {
+        if (!props.userSpeaking){
+            recorderControls.stopRecording()
+        }
+      }, [props.userSpeaking]) 
+        
+
+
+    return (
+        <div>
+            <AudioRecorder 
+                onRecordingComplete={(blob) => addAudioElement(blob)}
+                recorderControls={recorderControls} 
+            />
+        </div>
+    )
+}
