3839267f34f434cec023e7f7c3afbee422552e73
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-01-24 12:48:01 +0900

    Update Tips.jsx
    Basic tips added (may expand later)

--- src/Pages/Tips.jsx
+++ src/Pages/Tips.jsx
@@ -5,7 +5,17 @@ function Tips() {
     <main>
       <div className='page-container'>
         <h1>TOEFL Independent Speaking Tips</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum ducimus quisquam cumque commodi quae beatae ab mollitia eligendi aspernatur? Impedit doloremque placeat aliquid sequi tenetur obcaecati illo ipsum voluptates consequatur.</p>
+        <p>The independent speaking tasks on the TOEFL require you to speak on a given topic for 45 seconds. Before you begin, you will have 15 seconds of preparation time to organize your thoughts. These tasks are designed to assess your ability to speak spontaneously and express your own ideas on a given topic.</p>
+        <p>Here are some tips to help you succeed on the independent speaking tasks:</p>
+        <ol>
+          <li><strong>Understand the task</strong>: Make sure you fully understand what you are being asked to do before you begin your preparation time.</li>
+          <li><strong>Organize your thoughts</strong>: Use your preparation time wisely to organize your thoughts and ideas. Jot down key points you want to make and the main ideas you want to express.</li>
+          <li><strong>Speak clearly and at a natural pace</strong>: Speak clearly and at a natural pace, making sure to use appropriate intonation and stress.</li>
+          <li><strong>Use examples and specific details</strong>: Use examples and specific details to support your points and make your response more interesting and coherent.</li>
+          <li><strong>Use transitions</strong>: Use transitional words and phrases to link your ideas and make your response more cohesive.</li>
+          <li><strong>Practice, Practice, Practice</strong>: The more you practice speaking on different topics, the more comfortable and confident you will be on test day.</li>
+        </ol>
+        <p>Remember, the independent speaking tasks are designed to assess your ability to express your own thoughts and ideas, so don't be afraid to let your personality shine through! Practice these tips and you'll be on your way to success on the independent speaking section of the TOEFL.</p>
       </div>
     </main>
   )
